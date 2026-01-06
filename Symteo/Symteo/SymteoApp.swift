204a905bcaee63597f4c0bcee15a9468e8de177e
jiwookim1202 <godblessjiwoo@gmail.com>
2026-01-06 19:30:19 +0900

    [Chore] 초기 폴더 세팅
    폴더 컨벤션에 따라서 초기 폴더 구조 작성

---
+++ Symteo/Symteo/SymteoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymteoApp.swift
+//  Symteo
+//
+//  Created by 김지우 on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct SymteoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
