d7910af47990e4989cb17fbcbb80175829b06225
jiwookim1202 <godblessjiwoo@gmail.com>
2026-01-15 17:38:36 +0900

    [feat] TabIcon 수정
    탭 아이콘 설정

--- Symteo/Symteo/Common/Enum/Tab/TabItem.swift
+++ Symteo/Symteo/Common/Enum/Tab/TabItem.swift
@@ -7,19 +7,39 @@
 
 enum TabItem: String, CaseIterable{
     
-    /// 홈 탭
-    case home = "Home"
+    case home, chat, profile, test, report
+
+
+    var title: String {
+            switch self {
+            case .home: return "홈"
+            case .chat: return "진단"
+            case .profile: return "리포트"
+            case .test: return "맞춤 상담"
+            case .report: return "My 심터"
+
+            }
+        }
     
-    /// 진단 탭
-    case test = "Test"
+    var systemIcon: String{
+        switch self{
+        case .home: return "home"
+        case .chat: return "test"
+        case .profile: return "report"
+        case .test: return "chat"
+        case .report: return "profile"
+
+        }
+    }
     
-    /// 리포트 탭
-    case report = "Report"
-    
-    /// 맞춤상담 탭
-    case chat = "Chat"
-    
-    /// MY심터 탭
-    case profile = "Profile"
-    
+    var systemIconFill: String{
+        switch self{
+        case .home: return "home_fill"
+        case .chat: return "test_fill"
+        case .profile: return "report_fill"
+        case .test: return "chat_fill"
+        case .report: return "profile_fill"
+
+        }
+    }
 }
