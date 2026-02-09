5f2c732a31bd5a0368cae83332e6390b20c23f5f
Byeongseon-Park <parkbsbsbsbsbs@gmail.com>
2026-02-09 16:21:33 +0900

    feat: CustomToastView, CustomToast 모델 추가

---
+++ Symteo/Symteo/Models/Domain/Common/CustomToast.swift
@@ -0,0 +1,12 @@
+//
+//  CustomToast.swift
+//  Symteo
+//
+//  Created by 박병선 on 2/9/26.
+//
+struct CustomToast: Equatable {
+    var title: String
+    var message: String
+    var duration: Double = 3
+}
+
